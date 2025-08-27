a18099b6324b79584bbb869b0ed7af34256eaad4
Owolabi Idris Yusuf <69768951+Owolabi5541@users.noreply.github.com>
2025-08-27 13:52:57 +0100

    Update Navbar.jsx
    Updated Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 import { CiMenuFries } from 'react-icons/ci'
 import { TfiClose } from 'react-icons/tfi'
 import { motion } from 'framer-motion'
-import resume from "../assets/Updated-Resume.pdf"
+import resume from "../assets/updated-resume.pdf"
 
 const Navbar = () => {
     const [drawer, setDrawer] = useState(false)
@@ -118,5 +118,6 @@ const Navbar = () => {
     </div>
   )
 }
+
 
 export default Navbar
